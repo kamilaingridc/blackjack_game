c16d2320555bb3d4d3e5b7ba4b4a35ffd0e213de
kamila <kamilaingridc@gmail.com>
2024-01-12 13:23:35 -0300

    updates

--- medium/blackjack.py
+++ medium/blackjack.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 
 class Jogadores:
@@ -6,6 +7,9 @@ class Jogadores:
         self.__nome = nome
         self.__idade = idade
         self.__fichas = fichas
+
+    def get_fichas(self):
+        return self.__fichas
 
 
 class Jogo:
@@ -43,34 +47,59 @@ class Jogo:
         self.iniciar_jogo()
 
     def distribuir_cartas(self):
-        random.shuffle(self.__baralho)
+        cartas_distribuidas = []
+
         for jogador in self.__lista_jogadores:
-            carta = self.__baralho.pop(0)
-            print(f"{jogador.nome} recebeu a carta {carta}")
+            carta = random.shuffle(self.__baralho)
+            jogador_cartas = [carta]
+            cartas_distribuidas.append((jogador, jogador_cartas))
+            print(f"{jogador._Jogadores__nome} recebeu a carta {carta}")
+
+        return cartas_distribuidas
 
     def exibir_cartas(self):
+        cartas_distribuidas = self.distribuir_cartas()
+
+        for jogador, cartas in cartas_distribuidas:
+            print(f"{jogador._Jogadores__nome} tem as cartas: {cartas}.")
+
+    def exibir_fichas(self):
         for jogador in self.__lista_jogadores:
-            print(f"{jogador.nome} tem {jogador.fichas} fichas.")
+            print(f"{jogador._Jogadores__nome} tem {jogador.get_fichas()} fichas.")
 
     def aposta(self):
-        if self.__fichas >0:
-            apostando = input("Quanto você vai querer apostar?")
-            
-        else:
-            print("Você não tem fichas suficientes.")
+        for i, jogador in enumerate(self.__lista_jogadores):
+            if jogador.get_fichas() > 0:
+                apostando = int(input(f"{jogador._Jogadores__nome}, quanto você vai querer apostar? "))
+                if 0 < apostando <= jogador.get_fichas():
+                    pass
+                else:
+                    print("Saldo insuficiente.")
+                    self.aposta()
+            else:
+                print(f"{jogador._Jogadores__nome}, você não tem fichas suficientes.")
+
+    def vencedor(self):
+        pass
+
+    def pontuacao(self):
         pass
 
     def pegar_cartas(self, jogador):
-        print(f"{jogador.nome} escolheu pegar cartas.")
+        print(f"{jogador._Jogadores__nome} escolheu pegar cartas.")
 
     def nao_pegar_cartas(self, jogador):
-        print(f"{jogador.nome} escolheu não pegar cartas.")
+        print(f"{jogador._Jogadores__nome} escolheu não pegar cartas.")
 
     def iniciar_jogo(self):
+        print("Vamos começar!!")
+        time.sleep(1)
+        self.aposta()
         self.distribuir_cartas()
         self.exibir_cartas()
-        for jogador in self.__lista_jogadores:
-            escolha = int(input(f"{jogador.nome}, escolha:\n"
+
+        for i, jogador in enumerate(self.__lista_jogadores):
+            escolha = int(input(f"{jogador._Jogadores__nome}, escolha:\n"
                                 "[1] Pegar cartas | [2] Não pegar cartas "))
             if escolha == 1:
                 self.pegar_cartas(jogador)
@@ -78,6 +107,5 @@ class Jogo:
                 self.nao_pegar_cartas(jogador)
 
 
-# Exemplo de uso
 jogo = Jogo()
 jogo.menu()
